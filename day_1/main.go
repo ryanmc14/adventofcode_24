a0e68e779d045f35cc5557488d385b6bf56e547b
Ryan Mcleavy <ryanmcleavy14@gmail.com>
2024-12-01 18:40:07 +0000

    gofmt

--- day_1/main.go
+++ day_1/main.go
@@ -11,10 +11,10 @@ import (
 )
 
 func main() {
-    var listLeft []int
-    var listRight []int
-    var part1Answer  int
-    var part2Answer  int
+	var listLeft []int
+	var listRight []int
+	var part1Answer int
+	var part2Answer int
 
 	sc := advent_utils.Input("input.txt")
 	re := regexp.MustCompile("   ")
@@ -30,39 +30,38 @@ func main() {
 		listRight = append(listRight, intRight)
 	}
 
-    sort.Ints(listLeft)
-    sort.Ints(listRight)
+	sort.Ints(listLeft)
+	sort.Ints(listRight)
 
-    //part 1
-    for i, _ := range listLeft {
-        part1Answer += diff(listLeft[i], listRight[i])
-    }
-    fmt.Println(part1Answer)
+	//part 1
+	for i, _ := range listLeft {
+		part1Answer += diff(listLeft[i], listRight[i])
+	}
+	fmt.Println(part1Answer)
 
-    //part2
-    for _, v := range listLeft{
-        frequency := uniqueValue(v, listRight)
-        part2Answer += (v*frequency)
-    }
+	//part2
+	for _, v := range listLeft {
+		frequency := uniqueValue(v, listRight)
+		part2Answer += (v * frequency)
+	}
 
-    fmt.Println(part2Answer)
+	fmt.Println(part2Answer)
 }
-
 
 func diff(a, b int) int {
-   if a < b {
-      return b - a
-   }
-   return a - b
+	if a < b {
+		return b - a
+	}
+	return a - b
 }
 
-func uniqueValue(value int, arr []int) int{
-    frequency:= 0
-    for _, num := range arr {
-        if num != value{
-            continue
-        }
-        frequency +=1
-    }
-    return frequency
+func uniqueValue(value int, arr []int) int {
+	frequency := 0
+	for _, num := range arr {
+		if num != value {
+			continue
+		}
+		frequency += 1
+	}
+	return frequency
 }
